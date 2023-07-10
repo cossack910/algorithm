b978f5e035a5ef71351a0d12d1b0db46673654db
cossack910 <maris160zhk@gmail.com>
2023-07-10 21:50:57 +0900

    マージソート

--- sort/sort.py
+++ sort/sort.py
@@ -3,21 +3,22 @@ from typing import List
 
 
 class InsertSort:
-    def insertSort(self, a: List):
-        n = len(a)
+    def __init__(self, a: List):
+        self.a = a
+
+    def insertSort(self):
+        n = len(self.a)
         for i in range(n):
-            v = a[i]  # 挿入したい値
+            v = self.a[i]  # 挿入したい値
 
             j = i
             for j_ in reversed(range(0, j)):
-                if a[j-1] > v:  # vより大きいものは１つ後ろに
-                    a[j] = a[j-1]
+                if self.a[j-1] > v:  # vより大きいものは１つ後ろに
+                    self.a[j] = self.a[j-1]
                 else:
                     break  # v以下になったら終了
                 j -= 1
-            a[j] = v
-
-        return a
+            self.a[j] = v
 
 
 class MergeSort:
@@ -27,7 +28,7 @@ class MergeSort:
     def mergeSort(self, left: int, right: int):
         if right - left == 1:
             return
-        mid = left + math.ceil((right - left) / 2)
+        mid = left + (right - left) // 2
 
         # 左半分ソート
         self.mergeSort(left, mid)
@@ -45,17 +46,13 @@ class MergeSort:
         index_left = 0
         index_right = len(buf) - 1
         for i in range(left, right):
-            if buf[index_left] < buf[index_right]:
-                index_left += 1
+            if buf[index_left] <= buf[index_right]:
                 self.a[i] = buf[index_left]
+                index_left += 1
             else:
-                index_right -= 1
                 self.a[i] = buf[index_right]
+                index_right -= 1
         print(self.a)
 
 
-arr = [12, 9, 15, 3, 8, 17, 6, 1]
-s = MergeSort(arr)
-s.mergeSort(0, len(arr)-1)
-print(s.a)
 # docker exec -it python3.10 python3 /var/www/php-py/algorithm/sort/sort.py
--- sort/testSort.py
+++ sort/testSort.py
@@ -1,10 +1,19 @@
 import pytest
 from sort import InsertSort
+from sort import MergeSort
 
 
 def testInsertSort():
-    s = InsertSort()
-    SS = s.insertSort([4, 1, 3, 5, 2])
-    assert SS == [1, 2, 3, 4, 5]
+    arr = [4, 1, 3, 5, 2]
+    s = InsertSort(arr)
+    s.insertSort()
+    assert s.a == sorted(arr)
+
+
+def testMergeSort():
+    arr = [12, 9, 15, 3, 8, 17, 6, 1]
+    s = MergeSort(arr)
+    s.mergeSort(0, len(arr))
+    assert s.a == sorted(arr)
 
 # docker exec -it python3.10 pytest /var/www/php-py/algorithm/sort/testSort.py
